84487df3274d4b296749451501f25316169ce516
xiangsong <1638392300@qq.com>
2019-12-15 13:15:54 +0800

    init repo

---
+++ src/main/java/com/xschen/community/majinag/MajinagApplication.java
@@ -0,0 +1,13 @@
+package com.xschen.community.majinag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajinagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajinagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xschen/community/majinag/MajinagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xschen.community.majinag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajinagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
