a910f062d01d9f56e218b287452730aa47887d11
474408578 <1638392300@qq.com>
2020-01-06 23:42:38 +0800

    add properties from code to properties file

--- src/main/java/com/xschen/community/majinag/controller/AuthorizeController.java
+++ src/main/java/com/xschen/community/majinag/controller/AuthorizeController.java
@@ -4,32 +4,51 @@ import com.xschen.community.majinag.dto.AccessTokenDTO;
 import com.xschen.community.majinag.dto.GithubUser;
 import com.xschen.community.majinag.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
 
-
+    // https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/
+    // 点击登录后，跳转至登录的URL页面进行github认证，认证成功后，会跳转至callback路由，此时会执行这个函数
     // 获取github返回的code和state值，并模拟http请求，
     // 调用github的access_token接口，拿到用户的access_token
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
-        accessTokenDTO.setClient_id("96514d36d3befe4a5c2f");
-        accessTokenDTO.setClient_secret("22486db2162e67a8be1fb54783fffa9ecba98e71");
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser.getName());
-        return "index";
+        if (githubUser != null){
+            // 登录成功，写入session和cookie
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            // 登录失败, 重新登录
+            return "redirect:/";
+        }
     }
 }
