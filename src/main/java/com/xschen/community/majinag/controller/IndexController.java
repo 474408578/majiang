e620791edc095790b90c0803ae6e502f1520941a
474408578 <1638392300@qq.com>
2020-01-09 23:53:07 +0800

    add findByToken

--- src/main/java/com/xschen/community/majinag/controller/IndexController.java
+++ src/main/java/com/xschen/community/majinag/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.xschen.community.majinag.controller;
 
+import com.xschen.community.majinag.mapper.UserMapper;
+import com.xschen.community.majinag.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -13,8 +19,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie: cookies) {
+            if (cookie.getName().equals("token")) {
+                String value = cookie.getValue();
+                User user = userMapper.findByToken(value);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
--- src/main/java/com/xschen/community/majinag/mapper/UserMapper.java
+++ src/main/java/com/xschen/community/majinag/mapper/UserMapper.java
@@ -3,9 +3,15 @@ package com.xschen.community.majinag.mapper;
 import com.xschen.community.majinag.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(account_id, name, token, gmt_create, gmt_modified) values (#{accountId}, #{name}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);//不是类的时候使用@Param将token的值赋给SQL语句的token
+
 }
